34d419c521854f3bee8a26101b08dc9e97fd6c0d
dj-luis198 <dj.luis198@gmail.com>
2023-03-25 13:07:39 -0300

    post and update cards

--- cypress/e2e/apiTrello.cy.js
+++ cypress/e2e/apiTrello.cy.js
@@ -1,106 +1,127 @@
 /// const { defineConfig } = require("cypress");
-const environmentQA= require('../support/environmentQA');
+const environmentQA = require('../support/environmentQA');
 
 describe('', () => {
-  let cardId1, cardId2,cardId3,posCard1,posCard2,posCard3;
+  let cardId1, cardId2, cardId3, posCard1, posCard2, posCard3,
+    listaA, listaB, listaC, idMember1, idMember2, label1, label2;
 
   before(`pre-condiciones: La lista "TO DO" debe estar disponible en el tablero
           La lista "IN PROGRESS" debe estar disponible en el tablero
           La lista "DONE" debe estar disponible en el tablero
           Todas las listas estan vacias
-          Hay 2 miempros agregados en el tablero`,()=>{
-
+          Hay 2 miempros agregados en el tablero`, () => {
     cy.request({
-      url: `${environmentQA.GET}/${environmentQA.LISTA}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'GET'
-    }).then(result=>{
+      url: `${environmentQA.GETBOARD}/${environmentQA.IDBOARD}/${environmentQA.LISTS}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'GET'
+    }).then(result => {
       cy.log(result);
       expect(result.status).to.eql(200);
-      expect(result.body.name).to.eql('TO DO');
-      expect(result.body.idBoard).to.eql(`${environmentQA.BOARD}`);
-      expect(result.body.closed).to.eql(false);
+      expect(result.body).to.have.length(3);
+      expect(result.body[0].name).to.eql('TO DO');
+      expect(result.body[0].closed).to.eql(false);
+      expect(result.body[1].name).to.eql('IN PROGRESS');
+      expect(result.body[1].closed).to.eql(false);
+      expect(result.body[2].name).to.eql('DONE');
+      expect(result.body[2].closed).to.eql(false);
+      listaA = result.body[0].id;
+      listaB = result.body[1].id;
+      listaC = result.body[2].id;
     })
     cy.request({
-      url: `${environmentQA.GET}/${environmentQA.LISTB}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'GET'
-    }).then(result2=>{
-      cy.log(result2);
-      expect(result2.status).to.eql(200);
-      expect(result2.body.name).to.eql('IN PROGRESS');
-      expect(result2.body.idBoard).to.eql(`${environmentQA.BOARD}`);
-      expect(result2.body.closed).to.eql(false);
+      url: `${environmentQA.GETBOARD}/${environmentQA.IDBOARD}/${environmentQA.CARDS}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'GET'
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+      expect(result.body).to.be.empty;
     })
     cy.request({
-      url: `${environmentQA.GET}/${environmentQA.LISTC}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'GET'
-    }).then(result3=>{
-      cy.log(result3);
-      expect(result3.status).to.eql(200);
-      expect(result3.body.name).to.eql('DONE');
-      expect(result3.body.idBoard).to.eql(`${environmentQA.BOARD}`);
-      expect(result3.body.closed).to.eql(false);
+      url: `${environmentQA.GETBOARD}/${environmentQA.IDBOARD}/${environmentQA.MEMBERSHIPS}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'GET'
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+      expect(result.body).to.have.length(3);
+      idMember1 = result.body[1].idMember;
+      idMember2 = result.body[2].idMember;
     })
     cy.request({
-      url: `${environmentQA.GETBOARD}/${environmentQA.BOARD}/${environmentQA.CARDS}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'GET'
-    }).then(result4=>{
-      cy.log(result4);
-      expect(result4.status).to.eql(200);
-      expect(result4.body).to.be.empty;
-    })
-    cy.request({
-      url: `${environmentQA.GETBOARD}/${environmentQA.BOARD}/${environmentQA.MEMBERSHIPS}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'GET'
-    }).then(result5=>{
-      cy.log(result5);
-      expect(result5.status).to.eql(200);
-      expect(result5.body).to.have.length(3);
+      url: `${environmentQA.GETBOARD}/${environmentQA.IDBOARD}/${environmentQA.LABELS}?key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'GET'
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+      label1 = result.body[0].id;
+      label2 = result.body[1].id;
     })
   })
 
   it('US 23269 | {API} Trello | Cards | Crear cards de un tablero', () => {
     cy.request({
-      url: `${environmentQA.POSTCARD}?idList=${environmentQA.LISTA}&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'POST',
-      body:{
-        name:'ejemplo1',
-      }, 
-    }).then(result1=>{
-      cy.log(result1);
-      expect(result1.status).to.eql(200);
-      cardId1= result1.body.id;
-      posCard1=result1.body.pos; 
+      url: `${environmentQA.POSTCARD}?idList=${listaA}&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'POST',
+      body: {
+        name: 'ejemplo1',
+      },
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+      cardId1 = result.body.id;
+      posCard1 = result.body.pos;
     })
     cy.request({
-      url: `${environmentQA.POSTCARD}?idList=${environmentQA.LISTA}&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'POST',
-      body:{
-        name:'ejemplo2',
-      }, 
-    }).then(result2=>{
-      cy.log(result2);
-      expect(result2.status).to.eql(200);
-      expect(result2.body.pos).to.be.greaterThan(posCard1);
-      cardId2= result2.body.id;
-      posCard2=result2.body.pos; 
+      url: `${environmentQA.POSTCARD}?idList=${listaA}&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'POST',
+      body: {
+        name: 'ejemplo2',
+      },
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+      expect(result.body.pos).to.be.greaterThan(posCard1);
+      cardId2 = result.body.id;
+      posCard2 = result.body.pos;
     })
     cy.request({
-      url: `${environmentQA.POSTCARD}?idList=${environmentQA.LISTA}&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
-      method:'POST',
-      body:{
-        name:'ejemplo3',
-      }, 
-    }).then(result3=>{
-      cy.log(result3);
-      expect(result3.status).to.eql(200);
-      expect(result3.body.pos).to.be.greaterThan(posCard2);
-      cardId3= result3.body.id;
-      posCard3= result3.body.pos; 
+      url: `${environmentQA.POSTCARD}?idList=${listaA}&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'POST',
+      body: {
+        name: 'ejemplo3',
+      },
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+      expect(result.body.pos).to.be.greaterThan(posCard2);
+      cardId3 = result.body.id;
+      posCard3 = result.body.pos;
     })
   })
 
-  it('',()=>{
-
+  it('US 23269 | {API} Trello | Cards | Modificar cards de un tablero', () => {
+    cy.request({
+      url: `${environmentQA.PUTCARD}/${cardId1}?&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'PUT',
+      body: {
+        name: 'ejemploModificado1',
+        desc: 'texto descripcion de card numero 1',
+        idMembers: `${idMember1}`,
+      },
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+    })
+    cy.request({
+      url: `${environmentQA.PUTCARD}/${cardId2}?&key=${environmentQA.KEY}&token=${environmentQA.TOKEN}`,
+      method: 'PUT',
+      body: {
+        name: 'ejemploModificado2',
+        desc: 'texto descripcion de card numero 2',
+        idMembers: `${idMember1},${idMember2}`,
+      },
+    }).then(result => {
+      cy.log(result);
+      expect(result.status).to.eql(200);
+    })
   })
-  
+
 })
