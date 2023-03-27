399e2688e1b27f26e3e348e57813e08bc968274e
dj-luis198 <dj.luis198@gmail.com>
2023-03-27 13:57:41 -0300

    order code

---
+++ cypress/e2e/apiTrello/postCards.cy.js
@@ -0,0 +1,136 @@
+/// <reference types="cypress" />
+const environmentQA = require('../../support/environmentQA');
+
+describe('Crear y eliminar tarjetas de un tablero', () => {
+    let cardId1, cardId2, cardId3, posCard1, posCard2, posCard3,
+        listaA, listaB, listaC, dataCard;
+
+    before(`pre-condiciones: La lista "TO DO" debe estar disponible en el tablero
+          La lista "IN PROGRESS" debe estar disponible en el tablero
+          La lista "DONE" debe estar disponible en el tablero
+          Todas las listas estan vacias`, () => {
+
+        cy.fixture('data').then(data => {
+            dataCard = data;
+        })
+        cy.getBoardData(environmentQA.IDBOARD, environmentQA.LISTS)
+            .then(result => {
+                cy.log(result);
+                expect(result.status).to.eql(200);
+                expect(result.body).to.have.length(3);
+                expect(result.body[0]).to.have.property('name', 'TO DO');
+                expect(result.body[0]).to.have.property('closed', false);
+                expect(result.body[1]).to.have.property('name', 'IN PROGRESS');
+                expect(result.body[1]).to.have.property('closed', false);
+                expect(result.body[2]).to.have.property('name', 'DONE');
+                expect(result.body[2]).to.have.property('closed', false);
+                listaA = result.body[0].id;
+                listaB = result.body[1].id;
+                listaC = result.body[2].id;
+            })
+        cy.getBoardData(environmentQA.IDBOARD, environmentQA.CARDS)
+            .then(result => {
+                cy.log(result);
+                expect(result.status).to.eql(200);
+                expect(result.body).to.be.empty;
+            })
+    })
+
+    it('US 23269 | {API} Trello | Cards | Validar crear primera card en la lista "TO DO" (name: corto)', () => {
+        cy.addCard(listaA, dataCard.name.corto)
+            .then(result => {
+                cy.log(result);
+                expect(result.status).to.eql(200);
+                cardId1 = result.body.id;
+                cy.getCard(result.body.id)
+                    .then(result1 => {
+                        cy.log(result1);
+                        expect(result1.status).to.eql(200);
+                        expect(result1.body).to.have.property('id', `${cardId1}`);
+                        expect(result1.body).to.have.property('name', dataCard.name.corto);
+                        expect(result1.body).to.have.property('idBoard', `${environmentQA.IDBOARD}`);
+                        expect(result1.body).to.have.property('idList', `${listaA}`);
+                        posCard1 = result1.body.pos;
+                    })
+            })
+    })
+
+    it('US 23269 | {API} Trello | Cards | Validar crear segunda card en la lista "TO DO" (name: null)', () => {
+
+        cy.addCard(listaA, dataCard.name.vacio)
+            .then(result => {
+                cy.log(result);
+                expect(result.status).to.eql(200);
+                cardId2 = result.body.id;
+                cy.getCard(result.body.id)
+                    .then(result1 => {
+                        cy.log(result1);
+                        expect(result1.status).to.eql(200);
+                        expect(result1.body).to.have.property('id', `${cardId2}`);
+                        expect(result1.body).to.have.property('name', dataCard.name.vacio);
+                        expect(result1.body.pos).to.be.greaterThan(posCard1);
+                        expect(result1.body).to.have.property('idBoard', `${environmentQA.IDBOARD}`);
+                        expect(result1.body).to.have.property('idList', `${listaA}`);
+                        posCard2 = result1.body.pos;
+                    })
+            })
+    })
+
+    it('US 23269 | {API} Trello | Cards | Validar crear tercera card en la lista "TO DO" (name: largo)', () => {
+
+        cy.addCard(listaA, dataCard.name.largo)
+            .then(result => {
+                cy.log(result);
+                expect(result.status).to.eql(200);
+                cardId3 = result.body.id;
+
+                cy.getCard(result.body.id)
+                    .then(result1 => {
+                        cy.log(result1);
+                        expect(result1.status).to.eql(200);
+                        expect(result1.body).to.have.property('id', `${cardId3}`);
+                        expect(result1.body).to.have.property('name', dataCard.name.largo);
+                        expect(result1.body.pos).to.be.greaterThan(posCard2);
+                        expect(result1.body).to.have.property('idBoard', `${environmentQA.IDBOARD}`);
+                        expect(result1.body).to.have.property('idList', `${listaA}`);
+                        posCard3 = result1.body.pos;
+                    })
+            })
+        })
+
+        after(() => {
+            cy.deleteCard(cardId1)
+              .then(result => {
+                cy.log(result);
+                expect(result.status).to.eql(200);
+                cy.getCard(cardId1)
+                  .then(result1 => {
+                    cy.log(result1);
+                    expect(result1.status).to.eql(404);
+                  })
+              })
+            cy.deleteCard(cardId2)
+                .then(result => {
+                    cy.log(result);
+                    expect(result.status).to.eql(200);
+                    cy.getCard(cardId2)
+                        .then(result1 => {
+                            cy.log(result1);
+                            expect(result1.status).to.eql(404);
+                        })
+                })
+            cy.deleteCard(cardId3)
+                .then(result2 => {
+                    cy.log(result2);
+                    expect(result2.status).to.eql(200);
+                    cy.getCard(cardId3)
+                        .then(result3 => {
+                            cy.log(result3);
+                            expect(result3.status).to.eql(404);
+                        })
+                })
+            })
+
+    })
+
+    
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+const environmentQA = require('../support/environmentQA');
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,113 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getBoardData', (idBoard,point) => {
+cy.request({
+    url: `${environmentQA.PBOARD}/${idBoard}/${point}`,
+    method: 'GET',
+    qs: {
+      key: `${environmentQA.KEY}`,
+      token: `${environmentQA.TOKEN}`,
+    }
+  })
+})
+
+Cypress.Commands.add('addCard', (idList, name) => {
+    cy.request({
+        url: `${environmentQA.PCARDS}`,
+        method: 'POST',
+        body: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+            idList: idList,
+            name: name,
+        },
+    })
+})
+
+Cypress.Commands.add('getCard', (idCard) => {
+    cy.request({
+        method: 'GET',
+        url: `${environmentQA.PCARDS}/${idCard}`,
+        failOnStatusCode: false,
+        qs: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+        },
+    })
+})
+
+Cypress.Commands.add('putCard', (idCard, name, desc, idMembers, idLabels) => {
+    cy.request({
+        url: `${environmentQA.PCARDS}/${idCard}`,
+        method: 'PUT',
+        body: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+            name: name,
+            desc: desc,
+            idMembers: idMembers.toString(),
+            idLabels: idLabels.toString(),
+        },
+    })
+})
+
+Cypress.Commands.add('putPosCard', (idCard, pos) => {
+    cy.request({
+        url: `${environmentQA.PCARDS}/${idCard}`,
+        method: 'PUT',
+        body: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+            pos: pos,
+        }
+    })
+})
+
+Cypress.Commands.add('putListCard', (idCard, idList, pos) => {
+    cy.request({
+        url: `${environmentQA.PCARDS}/${idCard}`,
+        method: 'PUT',
+        body: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+            idList: idList,
+            pos: pos,
+        }
+    })
+})
+
+Cypress.Commands.add('getList', (idList) => {
+    cy.request({
+        method: 'GET',
+        url: `${environmentQA.PLISTS}/${idList}/${environmentQA.CARDS}`,
+        qs: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+        },
+    })
+})
+
+Cypress.Commands.add('closedCard', (idCard, boolean) => {
+    cy.request({
+        url: `${environmentQA.PCARDS}/${idCard}`,
+        method: 'PUT',
+        body: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+            closed: boolean,
+        },
+    })
+})
+
+Cypress.Commands.add('deleteCard', (idCard) => {
+    cy.request({
+        url: `${environmentQA.PCARDS}/${idCard}`,
+        method: 'DELETE',
+        body: {
+            key: `${environmentQA.KEY}`,
+            token: `${environmentQA.TOKEN}`,
+        },
+    })
+})
